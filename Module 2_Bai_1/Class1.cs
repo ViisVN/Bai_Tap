72e597d58c3d6b8281d4ffe3db719aa3418b01c9
HUYNH NHAT HAO <haohn.124010121038@vtc.edu.vn>
2022-03-03 13:51:07 +0700

    Module2_Bai_1

---
+++ Module 2_Bai_1/Class1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nhà_Xe
+{
+    public class Chuyen_xe
+    {
+        private string maSochuyen;
+        private string hotentaixe;
+        private int soxe;
+
+        public Chuyen_xe(string maSochuyen, string hotentaixe, int soxe)
+        {
+            this.maSochuyen = maSochuyen;
+            this.hotentaixe = hotentaixe;
+            this.soxe = soxe;
+        }
+        public void info()
+        {
+            Console.WriteLine("Ma so chuyen: " + this.maSochuyen);
+            Console.WriteLine("Ho ten tai xe: " + this.hotentaixe);
+            Console.WriteLine("So xe: " + this.soxe);
+        }
+
+
+    }
+    public class Tuyen_Xe_Noi_Thanh : Chuyen_xe
+    {
+        private int so_tuyen;
+        private int km_diduoc;
+        public Tuyen_Xe_Noi_Thanh(string maSochuyen, string hotentaixe, int soxe, int so_tuyen, int km_diduoc) : base(maSochuyen, hotentaixe, soxe)
+        {
+            this.so_tuyen = so_tuyen;
+            this.km_diduoc = km_diduoc;
+        }
+        public void Info()
+        {
+            info();
+            Console.WriteLine("So tuyen: " + this.so_tuyen);
+            Console.WriteLine("Km di duoc: " + this.km_diduoc);
+            Console.WriteLine(getdoanhthu());
+        }
+        public double setdoanhthu()
+        {
+            return 0;
+        }
+        public double getdoanhthu()
+        {
+            return this.km_diduoc * 10000;
+        }
+    }
+
+    public class Tuyen_Xe_Ngoai_Thanh : Chuyen_xe
+    {
+        private string noi_den;
+        private int ngay_di_duoc;
+
+        public Tuyen_Xe_Ngoai_Thanh(string maSochuyen, string hotentaixe, int soxe, string noi_den, int ngay_di_duoc) : base(maSochuyen, hotentaixe, soxe)
+        {
+            this.noi_den = noi_den;
+            this.ngay_di_duoc = ngay_di_duoc;
+        }
+        public void Info()
+        {
+            info();
+            Console.WriteLine("Noi den: " + this.noi_den);
+            Console.WriteLine("Ngay di duoc: "+this.ngay_di_duoc);
+            Console.WriteLine(getdoanhthu());
+        }
+        public double setdoanhthu()
+        {
+            return 0;
+        }
+
+        public double getdoanhthu()
+        {
+            return this.ngay_di_duoc * 300000;
+        }    
+            
+    }
+}
---
+++ Module 2_Bai_1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nhà_Xe
+{
+    class program
+    {
+        public static void Main(string[] ars)
+        {
+            //Nhap thong tin
+            Tuyen_Xe_Ngoai_Thanh Ngoai1 = new Tuyen_Xe_Ngoai_Thanh("A197H", "Tran Van Lam", 7734, "Ha Noi", 3);
+            Tuyen_Xe_Ngoai_Thanh Ngoai2 = new Tuyen_Xe_Ngoai_Thanh("A91AC", "Dinh Van Tai", 7734, "Hai Phong", 3);
+
+            Tuyen_Xe_Noi_Thanh Noi1 = new Tuyen_Xe_Noi_Thanh("B351H", "Tran Van Lam", 3342, 7, 30);
+            Tuyen_Xe_Noi_Thanh Noi2 = new Tuyen_Xe_Noi_Thanh("B13A3", "Dinh Van Tai", 3342, 1, 15);
+
+            //Xuat ra thong tin
+            Ngoai1.Info();
+            Console.WriteLine();
+            Ngoai2.Info();
+            Console.WriteLine();
+            Noi1.Info();
+            Console.WriteLine();
+            Noi2.Info();
+            Console.WriteLine();
+
+            //Tong doanh thu
+            Console.WriteLine("Doanh thu noi thanh: {0,-1} ", (Noi1.getdoanhthu() + Noi2.getdoanhthu()));
+            Console.WriteLine("Doanh thu ngoai thanh: {0,-1} ", (Ngoai1.getdoanhthu() + Ngoai2.getdoanhthu()));
+            Console.WriteLine("Doanh thu: {0,-1} ", (Ngoai1.getdoanhthu() + Ngoai2.getdoanhthu() +Noi1.getdoanhthu() + Noi2.getdoanhthu()));
+
+
+
+        }
+    }    
+}
